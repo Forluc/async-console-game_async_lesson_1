ed7d8a3946f2ebbfa3bc36997d084574b6c686bf
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-12-28 21:23:30 +0400

    The frames in the animation now change every two bars

--- animation/starship.py
+++ animation/starship.py
@@ -2,32 +2,42 @@ import asyncio
 import os
 
 from animation.curses_tools import draw_frame, get_frame_size, read_controls
+from itertools import cycle
 
 
-async def animate_starship(canvas, start_row, start_column):
+def get_rockets():
     rocket_dir = os.path.join('animation', 'rocket')
-
+    rockets = []
     with open(os.path.join(rocket_dir, 'rocket_frame_1.txt'), 'r') as rocket:
-        active_frame = rocket.read()
+        rockets.append(rocket.read())
     with open(os.path.join(rocket_dir, 'rocket_frame_2.txt'), 'r') as rocket:
-        next_frame = rocket.read()
+        rockets.append(rocket.read())
 
-    frame_height, frame_width = get_frame_size(active_frame)
+    return rockets
+
+
+def twice_cycle(iterable):
+    for item in cycle(iterable):
+        for _ in range(2):
+            yield item
+
+
+async def animate_starship(canvas, row, column):
+    rockets = get_rockets()
+
+    frame_height, frame_width = get_frame_size(rockets[0])
     canvas_height, canvas_width = canvas.getmaxyx()
+
     border_width = 1
-
-    row, column = start_row + 1, start_column - round(frame_width / 2)
-
-    motion_space_width = canvas_width - frame_width - border_width
     motion_space_height = canvas_height - frame_height - border_width
+    motion_space_width = canvas_width - frame_width - border_width
 
-    while True:
-        draw_frame(canvas, row, column, active_frame, negative=True)
+    for frame in twice_cycle(rockets):
         row_offset, column_offset, space_pressed = read_controls(canvas)
 
-        row = min(row + row_offset, motion_space_height) if row_offset > 0 else max(row + row_offset, 1)
-        column = min(column + column_offset, motion_space_width) if column_offset > 0 else max(column + column_offset, 1)
+        row = max(1, min(row + row_offset, motion_space_height))
+        column = max(1, min(column + column_offset, motion_space_width))
 
-        draw_frame(canvas, row, column, next_frame)
-        active_frame, next_frame = next_frame, active_frame
+        draw_frame(canvas, row, column, frame)
         await asyncio.sleep(0)
+        draw_frame(canvas, row, column, frame, negative=True)
