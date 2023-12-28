aa93d7dfc0ade24390107475ac80c84a06c9a323
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-12-28 20:22:05 +0400

    Relocate randint from def blink to def get_stars

--- animation/stars.py
+++ animation/stars.py
@@ -3,22 +3,24 @@ import curses
 from random import choice, randint
 
 
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, row, column, symbol='*', offset_tics=None):
+    if offset_tics is None:
+        offset_tics = [20, 3, 5, 3]
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(randint(0, 20)):
+        for _ in range(offset_tics[0]):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(randint(0, 3)):
+        for _ in range(offset_tics[1]):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(randint(0, 5)):
+        for _ in range(offset_tics[2]):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(randint(0, 3)):
+        for _ in range(offset_tics[3]):
             await asyncio.sleep(0)
 
 
@@ -26,5 +28,9 @@ def get_stars(canvas, row, column):
     symbols = ['+', '*', '.', ':']
     number_of_stars = randint(50, 200)
 
-    return [blink(canvas, randint(0, row - 1), randint(0, column - 1), choice(symbols)) for _ in
-            range(number_of_stars)]
+    return [blink(canvas,
+                  randint(0, row - 1),
+                  randint(0, column - 1),
+                  choice(symbols),
+                  [randint(0, num) for num in [20, 3, 5, 3]])
+            for _ in range(number_of_stars)]
